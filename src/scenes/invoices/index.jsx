82b10a5542a8461ecdc640cda044dad02084a165
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-04-01 00:07:36 -0700

    invoices page rendering

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,29 +1,20 @@
-import { Box } from "@mui/material";
-import { DataGrid, GridToolbar} from "@mui/x-data-grid";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataContacts } from "../../data/mockData";
+import { mockDataInvoices } from "../../data/mockData";
 
 import Header from "../../components/Header";
-import { useTheme } from '@mui/material';
 
-const Contacts = () => {
+const Invoices = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
-    { field: "id", headerName: "ID", flex: 0.5 },
-    { field: "registrarId", headerName: "Registrar ID"},
+    { field: "id", headerName: "ID" },
     {
       field: "name",
       headerName: "Name",
       flex: 1,
       cellClassName: "name-column--cell",
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      headerAlign: "left",
-      align: "left",
     },
     {
       field: "phone",
@@ -36,25 +27,25 @@ const Contacts = () => {
       flex: 1,
     },
     {
-        field: "address",
-        headerName: "Address",
-        flex: 1,
-      },
-      {
-        field: "city",
-        headerName: "City",
-        flex: 1,
-      },
-      {
-        field: "zipCode",
-        headerName: "ZipCode",
+    field: "cost",
+    headerName: "Cost",
+    flex: 1,
+    renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+            ${params.row.cost}
+        </Typography>
+    )
+    },
+    {
+        field: "date",
+        headerName: "Date",
         flex: 1,
       },
   ];
 
   return (
     <Box m="20px">
-      <Header title="CONTACTS" subtitle="Lists of Contacts for future reference" />
+      <Header title="INVOICES" subtitle="Lists of Invoice Balances" />
       <Box
         m="40px 0 0 0"
         height="75vh"
@@ -89,13 +80,13 @@ const Contacts = () => {
       >
         <DataGrid 
           checkboxSelection 
-          rows={mockDataContacts} 
+          rows={mockDataInvoices} 
           columns={columns}
-          components={{ toolbar: GridToolbar}} 
+          components={{toolbar: GridToolbar}}
         />
       </Box>
     </Box>
   );
 };
 
-export default Contacts;
+export default Invoices;
