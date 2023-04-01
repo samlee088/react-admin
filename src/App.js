77d974cec20583048c79b7b55985925831899734
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-03-31 23:31:21 -0700

    contacts team table renders

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import TopBar from './scenes/global/TopBar'
 import SideBar from './scenes/global/SideBar';
 import Dashboard from './scenes/dashboard';
-// import Team from './scenes/team';
+import Team from './scenes/team';
 // import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -30,16 +30,16 @@ function App() {
               <TopBar/>
               <Routes>
                 <Route path='/'element={<Dashboard/>}/>
-                {/* <Route path='/'element={<Team/>}/> */}
-                {/* <Route path='/'element={<Contacts/>}/> */}
-                {/* <Route path='/'element={<Invoices/>}/> */}
-                {/* <Route path='/'element={<Form/>}/> */}
-                {/* <Route path='/'element={<Bar/>}/> */}
-                {/* <Route path='/'element={<Pie/>}/> */}
-                {/* <Route path='/'element={<Line/>}/> */}
-                {/* <Route path='/'element={<FAQ/>}/> */}
-                {/* <Route path='/'element={<Geography/>}/> */}
-                {/* <Route path='/'element={<Calendar/>}/> */}
+                <Route path='/team'element={<Team/>}/>
+                <Route path='/contacts'element={<Contacts/>}/>
+                {/* <Route path='/invoices'element={<Invoices/>}/> */}
+                {/* <Route path='/form'element={<Form/>}/> */}
+                {/* <Route path='/bar'element={<Bar/>}/> */}
+                {/* <Route path='/pie'element={<Pie/>}/> */}
+                {/* <Route path='/line'element={<Line/>}/> */}
+                {/* <Route path='/faq'element={<FAQ/>}/> */}
+                {/* <Route path='/geography'element={<Geography/>}/> */}
+                {/* <Route path='/calendar'element={<Calendar/>}/> */}
 
               </Routes>
             </main>
